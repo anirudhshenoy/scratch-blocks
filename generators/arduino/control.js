b662c40906239895b0d88576bd2057475cd02394
anirudhshenoy <anirudhshenoy92@gmail.com>
2018-07-20 00:41:31 +0530

    Added operator generators for arduino

--- generators/arduino/control.js
+++ generators/arduino/control.js
@@ -27,3 +27,32 @@ goog.require('Blockly.Arduino');
   return code;
 };
 
+/**
+ * Code generator for the repeat forever Arduino block.
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Completed code.
+ */
+
+Blockly.Arduino['control_forever'] = function(block) {
+ 
+  var branch = Blockly.Arduino.statementToCode(block, 'SUBSTACK');
+  Blockly.Arduino.addLoop('',branch + '\n');
+  return '';
+};
+
+/**
+ * Code generator for the if Arduino block.
+ * Arduino code: loop { delay(X); }
+ * @param {!Blockly.Block} block Block to generate the code from.
+ * @return {string} Completed code.
+ */
+Blockly.Arduino['control_if'] = function(block) {
+  var argument = Blockly.Arduino.valueToCode(block, 'CONDITION',
+      Blockly.Arduino.ORDER_NONE) || 'false';
+  var sub = Blockly.Arduino.valueToCode(block, 'SUBSTACK',
+      Blockly.Arduino.ORDER_NONE) || 'false';
+  var code = "if(" + argument + "){\n";
+  code += sub;
+  code += "\n}";
+  return code;
+}
---
+++ generators/arduino/text.js
@@ -0,0 +1,21 @@
+/**
+ * @license Licensed under the Apache License, Version 2.0 (the "License"):
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/**
+ * @fileoverview Generating Arduino code for the Math blocks.
+ *
+ * TODO: Math on list needs lists to be implemented.
+ *       math_constant and math_change needs to be tested in compiler.
+ */
+'use strict';
+
+goog.provide('Blockly.Arduino.texts');
+
+goog.require('Blockly.Arduino');
+
+Blockly.Arduino['text'] = function(block) {
+  var code = Blockly.Arduino.quote_(block.getFieldValue('TEXT'));
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
